93e85e1174006487fb8cab313a9badc6304bb836
Rene Torres <rene_edgardo_2@hotmail.com>
2020-07-09 00:31:02 -0600

    Saving and viewing notes with Local Storage

--- Notes/index.js
+++ Notes/index.js
@@ -1,1 +1,18 @@
-console.log('working fine')
+window.onload = function(){ 
+    notesTextArea = document.getElementById('notes-textarea');
+    notesTextArea.addEventListener('keyup', saveNotes);
+    console.log(notesTextArea);
+
+    if (localStorage.getItem("notes") === null || localStorage.getItem("notes") === "") {
+        console.log('Its on if');
+        saveNotes(); 
+    }
+    else{
+        console.log('Its on else');
+        notesTextArea.textContent = localStorage.getItem('notes');
+    }
+};
+
+function saveNotes(){
+    localStorage.setItem('notes', notesTextArea.value);
+}  
